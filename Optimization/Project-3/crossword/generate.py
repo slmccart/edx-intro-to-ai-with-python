6bf57e27a7f95319eaf48864a86e5505d156cb32
Scott McCarthy <slmccart@gmail.com>
2024-07-21 11:31:08 -0400

    Implement enforce_node_consistency

--- Optimization/Project-3/crossword/generate.py
+++ Optimization/Project-3/crossword/generate.py
@@ -103,7 +103,15 @@ class CrosswordCreator:
         (Remove any values that are inconsistent with a variable's unary
          constraints; in this case, the length of the word.)
         """
-        raise NotImplementedError
+        # Loop over each variable (set of blank spaces)
+        for variable in self.domains:
+            # Loop over a copy of the domain for each variable, since we cannot modify
+            #   the set we are looping over
+            for word in self.domains[variable].copy():
+                # If a word in the domain does not match the length of the variable,
+                #   remove the word from the domain
+                if len(word) != variable.length:
+                    self.domains[variable].remove(word)
 
     def revise(self, x, y):
         """
