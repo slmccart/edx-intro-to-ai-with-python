4965dbe18432fc3755436bd55d6ede04380bd36b
Scott McCarthy <slmccart@gmail.com>
2024-07-21 15:45:00 -0400

    Adjust assertion logic for testing enforce_node_consistency

--- Optimization/Project-3/crossword/test_generate.py
+++ Optimization/Project-3/crossword/test_generate.py
@@ -5,11 +5,27 @@ from crossword import Crossword
 def test_enforce_node_consistency():
     cc = CrosswordCreator(Crossword("data/structure0.txt", "data/words0.txt"))
 
-    cc.enforce_node_consistency()
+    found_mismatch = False
 
     for domain in cc.domains:
         for word in cc.domains[domain]:
-            assert len(word) == domain.length
+            if len(word) != domain.length:
+                found_mismatch = True
+                break
+
+    assert found_mismatch
+
+    cc.enforce_node_consistency()
+
+    found_mismatch = False
+
+    for domain in cc.domains:
+        for word in cc.domains[domain]:
+            if len(word) != domain.length:
+                found_mismatch = True
+                break
+
+    assert not found_mismatch
 
 
 if __name__ == "__main__":
